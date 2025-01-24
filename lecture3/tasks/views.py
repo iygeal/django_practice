3f3c1bb2252eb120dccaedcddc2e9490990ca9c0
Innocent Anozie <iygeal@gmail.com>
2025-01-24 09:30:58 +0100

    Added a condition to tasks app

--- lecture3/tasks/views.py
+++ lecture3/tasks/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
+# Global variables
+tasks = ["foo", "bar", "baz"]
+
 # Create your views here.
+
+
+def index(request):
+    """Defines the index page"""
+    return render(request, "tasks/index.html", {
+        "tasks": tasks
+    })
