936881fae857c9ac677b8bdcbd376cd39197f3bd
Innocent Anozie <iygeal@gmail.com>
2025-01-25 08:11:03 +0100

    UPDATED: Removed tasks as global variable and instead make it a session variable

--- lecture3/tasks/urls.py
+++ lecture3/tasks/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
 from . import views
 
+
+# Give the routes/URLS for the tasks app a name
+app_name = "tasks"
+
 urlpatterns = [
     path("", views.index, name="index"),
     path("add", views.add, name="add")
--- lecture3/tasks/views.py
+++ lecture3/tasks/views.py
@@ -1,18 +1,40 @@
 from django.shortcuts import render
+from django import forms
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
-# Global variables
-tasks = ["foo", "bar", "baz"]
+class NewTaskForm(forms.Form):
+    """Class to create a new task form"""
+    task = forms.CharField(label="New Task")
+
+
 
 # Create your views here.
 
-
 def index(request):
     """Defines the index page"""
+
+    if "tasks" not in request.session:
+        request.session["tasks"] = []
+
     return render(request, "tasks/index.html", {
-        "tasks": tasks
+        "tasks": request.session["tasks"]
     })
 
 
 def add(request):
     """Defines the add page"""
-    return render(request, "tasks/add.html")
+    if request.method == "POST":
+        form = NewTaskForm(request.POST)
+        if form.is_valid():
+            task = form.cleaned_data["task"]
+            tasks.append(task)
+            return HttpResponseRedirect(reverse("tasks:index"))
+        else:
+            return render(request, "tasks/add.html", {
+                "form": form
+            })
+
+    return render(request, "tasks/add.html", {
+        "form": NewTaskForm()
+    })
